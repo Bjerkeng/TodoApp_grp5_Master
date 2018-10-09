45d03a00b368051fe982febfca3b36789d3947b6
Lars-Otto Bjerkeng <Bjerkeng@live.com>
2018-10-09 12:51:24 +0200

    system i server.js og lagt til auth fil

--- server.js
+++ server.js
@@ -7,22 +7,26 @@ ourServer.set('port', port);
 ourServer.use(express.static('public'));
 ourServer.use(bodyParser.json());
 
-/*
-const users = [];
-ourServer.post("/api/user",function(req, res)){
-               let user = req.body;
-               user.id = usrs.length +1;
-               users.push(user);
-res.json(user).end()
-               }
-*/
+ourServer.listen(ourServer.get('port'), function () {
+    console.log('server running', ourServer.get('port'));
+});
+
+//Modules
+//----------------------------------------------------------------------------
+
+const authorize = require("./authorize.js");
+
+
+//Post, Get, list
+//----------------------------------------------------------------------------
+                          
+ourServer.GET("/api/list/:listID", auth ,function (req, res) {
+    //return list.
+});
+
 
 ourServer.post("/api/user",function (req, res) {
     res.send(req.body.fornavn).end();
-});
-
-ourServer.listen(ourServer.get('port'), function () {
-    console.log('server running', ourServer.get('port'));
 });
 
 
