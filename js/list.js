26ddf306100241a81ad203d94465d7c4f7b9bfed
Victoria Solberg <ikketoria@icloud.com>
2018-11-20 13:08:52 +0100

    litt endring i list og css

---
+++ js/list.js
@@ -0,0 +1,8 @@
+var express = require('express;');
+var router = express.Router();
+var db = require("./db.js")
+
+router.get('/app/posts', function(req, res, next){
+    let query = "";
+    let
+})
