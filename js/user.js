2a5f0341a678c60dacc113d4f11c526179920ac9
Lars-Otto Bjerkeng <bjerkeng@live.com>
2018-11-26 03:53:12 +0100

    Update user.js

--- js/user.js
+++ js/user.js
@@ -56,40 +56,6 @@ router.get('/app/user/:userName', function(req,res,next){
     //let code = db.insert(query) ? 200:500;
     //res.status(code).json({}).end();
 })
-/*
-router.post('/app/users', function(req,res,next){
-    //let checkId = ??;
-    //let checkEmail = req.body.email;
-    let updatePassword = req.body.passWord;
-    
-    let query = `UPDATE user SET hash = ${updatePassword} WHERE email = ${checkId}`;
-    
-    let code = db.update(query) ? 200:500;
-    res.status(code).json({}).end();
- 
- 
- router.delete('/delete/users', function(req,res,next){
-    
-    let query = `UPDATE user SET hash = ${updatePassword} WHERE email = ${checkId}`;
-    
-    let code = db.update(query) ? 200:500;
-    res.status(code).json({}).end();
-    
-}
-    
-router.post('/app/users', function(req,res,next){
-    let checkId = req.body.id;
-    let checkEmail = req.body.email;
-    let updatePassword = req.body.passWord;
-    
-    let query = 
-    `UPDATE user SET hash = 0, WHERE email = ${checkId}`
-    `INSERT INTO "public"."Delted-Users"("email", "username", "hash", "role") VALUES('${userEmail}','${userName}', '${passwordHash}', ${userRole}) RETURNING "id", "email", "hash", "role"`
-    ;
-    
-    let code = db.update(query) ? 200:500;
-    res.status(code).json({}).end();    
-})*/
 
 const user = {}
 module.exports = router;
